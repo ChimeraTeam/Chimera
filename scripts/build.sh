9319ff29feb5dd35308be7e4cb9741e9d0f1855f
gleb <mendrul.gleb@gmail.com>
2015-12-06 16:27:13 +0200

    fixed build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,15 @@
-$TOMCAT_HOME/bin/shutdown.sh
+cd $PROJECT_HOME
+git pull
 
-cd ..
 mvn clean
 mvn install -DskipTests
+
+$TOMCAT_HOME/bin/shutdown.sh
 
 rm -rf $TOMCAT_HOME/webapps/chimera_*
 cp chimera_web/target/chimera_web.war chimera_service/target/chimera_service.war $TOMCAT_HOME/webapps
 
 yes | cp -i chimera_processing/lib/xz-1.0.jar $TOMCAT_HOME/lib
 yes | cp -i chimera_service/src/main/webapp/WEB-INF/classes/log4j.properties $TOMCAT_HOME/lib
- 
+
 $TOMCAT_HOME/bin/startup.sh
-
-
-
-
