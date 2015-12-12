bd18fc3b602d8fbd4179025d0a744c358136f514
gleb <mendrul.gleb@gmail.com>
2015-12-12 15:15:55 +0200

    fixed build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,5 @@ rm -rf $TOMCAT_HOME/webapps/chimera_*
 cp chimera_web/target/chimera_web.war chimera_service/target/chimera_service.war $TOMCAT_HOME/webapps
 
 yes | cp -i chimera_processing/lib/xz-1.0.jar $TOMCAT_HOME/lib
-yes | cp -i chimera_service/src/main/webapp/WEB-INF/classes/log4j.properties $TOMCAT_HOME/lib
 
 $TOMCAT_HOME/bin/startup.sh
