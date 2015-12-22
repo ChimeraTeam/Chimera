15a3e055e69bede0ca97c9e8187193d9c9e6f9f0
yurayurakov <yura_yura_kov@i.ua>
2015-12-22 14:02:54 +0200

    Revert "fixed issue with new phase values"
    This reverts commit c01c6232ff6bab3493c56c9b997a7cb7ea289aeb.

--- chimera_service/src/main/webapp/js/BuildModelProcessor/Strategies/PhaseVisualizationStrategy.js
+++ chimera_service/src/main/webapp/js/BuildModelProcessor/Strategies/PhaseVisualizationStrategy.js
@@ -14,9 +14,7 @@
 
         for (var i = 0; i < data.length; i++) {
             colors[i] = new THREE.Color();
-			var phase = Math.abs(data[i]);
-			var newPhase = phase - parseInt(phase / (2 * Math.PI)) * 2 * Math.PI;
-            H = 3 * newPhase / 3.1427;
+            H = 3 * data[i] / 3.1427;
             Vm = V * (100 - S) / 100;
             a = (V - Vm) * (H % 1) / 60;
             Vi = Vm + a;
