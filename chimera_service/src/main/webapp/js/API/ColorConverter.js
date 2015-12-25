9ffa5a778bfe1b75f5fe023b07e067b34bbbe940
yurakoval93 <yura_yura_kov@i.ua>
2015-12-25 21:23:11 +0200

    phase visualization strategy refactoring

--- chimera_service/src/main/webapp/js/API/ColorConverter.js
+++ chimera_service/src/main/webapp/js/API/ColorConverter.js
@@ -1,9 +1,18 @@
 ﻿this.ColorConverter = function () {
-    this.convertHsvToHsl = function (hue, sat, val) {
-        return {
-            h : hue,
-            s : sat * val / ((hue = (2 - sat) * val) < 1 ? hue : 2 - hue),
-            l : hue / 2
+    this.hsv_to_hsl = function (h, s, v) {
+        // both hsv and hsl values are in [0, 1]
+        var l = (2 - s) * v / 2;
+
+        if (l != 0) {
+            if (l == 1) {
+                s = 0
+            } else if (l < 0.5) {
+                s = s * v / (l * 2)
+            } else {
+                s = s * v / (2 - l * 2)
+            }
         }
+
+        return [h / 100, s, l];
     }
 }
--- chimera_service/src/main/webapp/js/BuildModelProcessor/Strategies/PhaseVisualizationStrategy.js
+++ chimera_service/src/main/webapp/js/BuildModelProcessor/Strategies/PhaseVisualizationStrategy.js
@@ -9,32 +9,15 @@
         colors = [];
 
         var H, Vm, a, Vi, Vd;
-        var S = (1 - this.min / this.max) * 100;
-        var V = (this.max * 60) / 3.6;
+        var S = 0.8;
+        var V = 1;
 
         for (var i = 0; i < data.length; i++) {
             colors[i] = new THREE.Color();
-            H = 3 * data[i] / Math.PI;
-            Vm = V * (100 - S) / 100;
-            a = (V - Vm) * (H % 1) / 60;
-            Vi = Vm + a;
-            Vd = V - a;
-            H = Math.floor(H);
-
-            switch (H) {
-                case 0: colors[i].setRGB(V, Vi, Vm);
-                    break;
-                case 1: colors[i].setRGB(Vd, V, Vm);
-                    break;
-                case 2: colors[i].setRGB(Vm, V, Vi);
-                    break;
-                case 3: colors[i].setRGB(Vm, Vd, V);
-                    break;
-                case 4: colors[i].setRGB(Vi, Vm, V);
-                    break;
-                case 5: colors[i].setRGB(V, Vm, Vd);
-                    break;
-            }
+            H = 180 * data[i] / Math.PI;
+            var converter = new ColorConverter();
+            var hsl = converter.hsv_to_hsl(H, S, V);
+            colors[i].setHSL(hsl[0], hsl[1], hsl[2]);
         }
 
         return colors;
