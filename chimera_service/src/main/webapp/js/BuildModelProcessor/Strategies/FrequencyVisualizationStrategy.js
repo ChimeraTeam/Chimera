838f78cb5719cc1c76cf3ca7d8a6ddcc4d9e7cd8
yurakoval93 <yura_yura_kov@i.ua>
2015-12-25 21:45:17 +0200

    added chimera data compression and fixed FrequencyVisualizationStrategy

--- chimera_service/src/main/webapp/js/BuildModelProcessor/Strategies/FrequencyVisualizationStrategy.js
+++ chimera_service/src/main/webapp/js/BuildModelProcessor/Strategies/FrequencyVisualizationStrategy.js
@@ -1,47 +1,22 @@
 ﻿FrequencyVisualizationStrategy = function () {
 
     var colors = [];
-    var divideArray = [];
 
     this.min;
     this.max;
 
     this.ConvertToColorMap = function (data) {
         colors = [];
-        divideArray = [];
 
         var rgbColors = new RGB();
-        var colorsCount = rgbColors.GetColorsCount();
-
-        var displacement = (this.max - this.min) / (colorsCount - 1);
-        for (var i = 0; i < colorsCount; i++) {
-            divideArray[i] = parseFloat(this.min) + i * displacement;
-        }
 
         for (var i = 0; i < data.length; i++) {
             colors[i] = new THREE.Color();
 
-            var colorIndex = Round(data[i]);
-            var color = rgbColors.GetColors(colorIndex);
+            var color = rgbColors.GetColors(data[i]);
             colors[i].setRGB(color.r, color.g, color.b);
         }
+
         return colors;
-    }
-
-    function Round(value) {
-
-        var index = 0;
-        var min = 100;
-
-        for (var i = 0; i < divideArray.length; i++) {
-            var diff = value - divideArray[i];
-
-            if (Math.abs(diff) < Math.abs(min)) {
-                min = diff;
-                index = i;
-            }
-        }
-
-        return index;
     }
 }
