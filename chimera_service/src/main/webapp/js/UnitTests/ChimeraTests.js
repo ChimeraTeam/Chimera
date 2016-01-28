ebffcc1e14405116d3838a609ed2590e44bcf19e
yurayurakov <yura_yura_kov@i.ua>
2016-01-28 12:55:40 +0200

    add unit tests for cookies API

--- chimera_service/src/main/webapp/js/UnitTests/ChimeraTests.js
+++ chimera_service/src/main/webapp/js/UnitTests/ChimeraTests.js
@@ -1,16 +1,26 @@
-function dataDecryptTest(data, count) {
+function dataDecryptServiceTest(data, count) {
 	var service = new DataDecryptedService();
-    var decryptArray = service.decryptData(data);
-
-    if (decryptArray.length == count)
-        return true;
-
-    return false;
+    QUnit.assert.equal(service.decryptData(data).length, count, 'decrypt data with ' + count + ' values');
 }
 
-test('dataDecryptTest()', function() {
-    ok(dataDecryptTest("5.456", 1), 'decrypt data with 1 value');
-    ok(dataDecryptTest("5.456,8.7852", 2), 'decrypt data with 2 values');
-    ok(dataDecryptTest("4.456,6.7852,1.2222", 3), 'decrypt data with 3 values');
-    ok(dataDecryptTest("4.456,6.7852,1.2222,4.456,6.7852,1.2222,4.456,6.7852,1.2222", 9), 'decrypt data with 9 values');
+function cookiesAPITest(name, value) {
+    var cookiesApi = new Cookies();
+    cookiesApi.setCookie(name, value);
+
+    var res = cookiesApi.getCookie(name);
+    cookiesApi.eraseCookie(name);
+
+    QUnit.assert.equal(res, value, name + ' with value ' + value + ' successfully saved and read from cookies');
+    QUnit.assert.equal(cookiesApi.getCookie(name), "", name + ' was successfully erased');
+}
+
+QUnit.test('dataDecryptServiceTest', function() {
+    dataDecryptServiceTest("5.456", 1);
+    dataDecryptServiceTest("5.456,8.7852", 2);
+    dataDecryptServiceTest("4.456,6.7852,1.2222", 3);
+    dataDecryptServiceTest("4.456,6.7852,1.2222,4.456,6.7852,1.2222,4.456,6.7852,1.2222", 9);
+})
+
+QUnit.test('cookiesAPITest', function() {
+    cookiesAPITest('test_cookie', 1);
 })
