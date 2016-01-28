f1c34ca096cba38c580b49eb36034eab2f831fce
yurayurakov <yura_yura_kov@i.ua>
2016-01-28 14:59:41 +0200

    more unit tests

--- chimera_service/src/main/webapp/js/UnitTests/ChimeraTests.js
+++ chimera_service/src/main/webapp/js/UnitTests/ChimeraTests.js
@@ -14,6 +14,29 @@ function cookiesAPITest(name, value) {
     QUnit.assert.equal(cookiesApi.getCookie(name), "", name + ' was successfully erased');
 }
 
+function uiTests() {
+    var emulator = new ControlsEmulator();
+    emulator.emulate();
+
+    var uiCreator = new UICreator();
+
+    var btnId = emulator.getControlsIds()[0];
+    uiCreator.setControlValue(btnId, 'TEST');
+    uiCreator.setDisabledButtonProperty(btnId, true);
+
+    QUnit.assert.equal(uiCreator.getControlValue(btnId), 'TEST', 'control value was successfully changed');
+    QUnit.assert.equal(document.getElementById(btnId).disabled, true, 'button was successfully disabled');
+}
+
+function pageGridTest(){
+    var pageGrid = new PageGrid();
+    pageGrid.createGrid(2000, 1000);
+
+    QUnit.assert.ok(pageGrid.getFrameX(10) > 0, 'x coordinate for cell number 10 was successfully build');
+    QUnit.assert.ok(pageGrid.getFrameY(20) > 0, 'y coordinate for cell number 20 was successfully build');
+    QUnit.assert.ok(pageGrid.getFrameY(50) == null, 'cell number 50 does not exists');
+}
+
 QUnit.test('dataDecryptServiceTest', function() {
     dataDecryptServiceTest("5.456", 1);
     dataDecryptServiceTest("5.456,8.7852", 2);
@@ -23,4 +46,12 @@ QUnit.test('dataDecryptServiceTest', function() {
 
 QUnit.test('cookiesAPITest', function() {
     cookiesAPITest('test_cookie', 1);
+})
+
+QUnit.test('uiTests', function() {
+    uiTests();
+})
+
+QUnit.test('pageGridTest', function() {
+    pageGridTest();
 })
---
+++ chimera_service/src/main/webapp/js/UnitTests/ControlsEmulator.js
@@ -0,0 +1,21 @@
+/**
+ * Created by p8h77m on 1/28/2016.
+ */
+
+ControlsEmulator = function () {
+    var controlsId = [];
+
+    this.emulate = function () {
+        var btn = document.createElement('BUTTON');
+        btn.id = 'testBtn'
+        btn.value = 'Ok';
+        btn.style.visibility = "hidden";
+        document.body.appendChild(btn);
+
+        controlsId.push(btn.id);
+    }
+
+    this.getControlsIds = function () {
+        return controlsId;
+    }
+};
