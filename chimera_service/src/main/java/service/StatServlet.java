fd996a45d5ecc8f14d0da84dfa253fb6512e0a85
gleb <mendrul.gleb@gmail.com>
2016-02-10 22:14:10 +0200

    added stat

--- chimera_service/src/main/java/service/StatServlet.java
+++ chimera_service/src/main/java/service/StatServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by gleb on 2/10/16.
