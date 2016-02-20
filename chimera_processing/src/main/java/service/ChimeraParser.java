11c5bc140398d784c314e6df59cba420276726f4
gleb <mendrul.gleb@gmail.com>
2016-02-20 23:57:16 +0200

    added compression

---
+++ chimera_processing/src/main/java/service/ChimeraParser.java
@@ -0,0 +1,71 @@
+package service;
+
+import constants.Compress;
+import constants.RegExps;
+import constants.Types;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by gleb on 08.10.15.
+ */
+public class ChimeraParser {
+
+    private Types type;
+    private Pattern pattern;
+    private Compress compress;
+
+    public ChimeraParser(Types type, Compress compress) {
+        this.type = type;
+        this.compress = compress;
+    }
+
+    public String process(String input) {
+        if (input.startsWith(";")) return null;
+        if (Types.FREQUENCY.equals(type)) pattern = Pattern.compile(RegExps.FREQUENCY.getValue());
+        if (Types.PHASE.equals(type)) pattern = Pattern.compile(RegExps.PHASE.getValue());
+        return parseValue(input);
+    }
+
+    private String parseValue(String input) {
+        StringBuilder data = new StringBuilder();
+        Matcher matcher = pattern.matcher(input);
+        String out;
+        int i = 0;
+        while (matcher.find()) {
+            i++;
+            if (Compress.S.equals(compress) && (i % compress.getCompressValue() != 0)) continue;
+            String value = matcher.group().split(" ")[type.equals(Types.FREQUENCY) ? 0 : 2];
+            if (type.equals(Types.FREQUENCY)) {
+                out = parseFrequency(value);
+                data.append(out).append(",");
+            } else {
+                out = parsePhase(value);
+                data.append(out).append(",");
+            }
+        }
+        return data.toString();
+    }
+
+    private String parseFrequency(String line) {
+        Double frequency = Double.valueOf(line);
+        return compressFrequency(frequency);
+    }
+
+    private String parsePhase(String line) {
+        Double phase = Math.abs(Double.valueOf(line));
+        Double actual = phase - ((int) (phase / (2 * Math.PI))) * 2 * Math.PI;
+        return compressPhase(actual);
+    }
+
+    private String compressFrequency(Double frequency) {
+        frequency += 1;
+        frequency *= 128;
+        return String.valueOf(frequency.intValue());
+    }
+
+    private String compressPhase(Double phase) {
+        return String.valueOf((new Double(180 * phase / Math.PI)).intValue());
+    }
+}
--- chimera_service/src/main/java/service/ChimeraWebSocket.java
+++ chimera_service/src/main/java/service/ChimeraWebSocket.java
@@ -1,12 +1,13 @@
 package service;
 
 
+import constants.Compress;
 import constants.Types;
 import org.apache.log4j.Logger;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.websocket.*;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 /**
@@ -17,6 +18,9 @@ import javax.websocket.server.ServerEndpoint;
 public class ChimeraWebSocket {
 
     private Logger logger = Logger.getLogger(ChimeraWebSocket.class);
+    private static final String[] OSCILLATIONS_100 = {"100x100x100", "8"};
+    private static final String[] OSCILLATIONS_200 = {"200x200x200", "64"};
+    private static final String[] OSCILLATIONS_400 = {"400x400x400", "512"};
 
     @OnMessage
     public void onMessage(String data, Session session) {
@@ -24,11 +28,15 @@ public class ChimeraWebSocket {
         new Thread(new Runnable() {
             @Override
             public void run() {
+
                 String fileName = data.split("_")[0];
                 String type = data.split("_")[1];
+                String compress = data.split("_")[2];
+
                 logger.info("Starting processing file=" + fileName + " type=" + type + " session=" + session.getId());
+
                 ChimeraReader reader = new ChimeraReader(fileName);
-                ChimeraFilter filter = new ChimeraFilter(Types.getEnum(type));
+                ChimeraParser filter = new ChimeraParser(Types.getEnum(type), getCompressValue(compress, fileName));
                 while (reader.hasNext()) {
                     String value = filter.process(reader.next());
                     if (value != null) {
@@ -39,6 +47,23 @@ public class ChimeraWebSocket {
                 logger.info("Processed successfully file=" + fileName + " type=" + type + " session=" + session.getId());
             }
         }).start();
+    }
+
+    private Compress getCompressValue(String compress, String fileName) {
+        Compress compressEnum = Compress.getEnum(compress);
+        if (compressEnum != null) {
+            if (fileName.contains(OSCILLATIONS_100[0])) {
+                compressEnum.setCompressValue(Integer.valueOf(OSCILLATIONS_100[1]));
+            } else if (fileName.contains(OSCILLATIONS_200[0])) {
+                compressEnum.setCompressValue(Integer.valueOf(OSCILLATIONS_200[1]));
+            } else if (fileName.contains(OSCILLATIONS_400[0])) {
+                compressEnum.setCompressValue(Integer.valueOf(OSCILLATIONS_400[1]));
+            } else {
+                compressEnum.setCompressValue(0);
+            }
+            return compressEnum;
+        }
+        return Compress.N;
     }
 
     @OnClose
