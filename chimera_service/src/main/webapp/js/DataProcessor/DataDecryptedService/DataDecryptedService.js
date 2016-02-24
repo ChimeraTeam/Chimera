3df13ffd2255ff5f96cfaccd4e670f1cb859cfb3
yurakoval93 <yura_yura_kov@i.ua>
2016-02-24 23:45:53 +0200

    fixed frequency visualization

--- chimera_service/src/main/webapp/js/DataProcessor/DataDecryptedService/DataDecryptedService.js
+++ chimera_service/src/main/webapp/js/DataProcessor/DataDecryptedService/DataDecryptedService.js
@@ -5,10 +5,22 @@
             return data.split(',');
         }
 
+        var readyData = [];
+        var parser = new FrequencyParser();
         var array = data.split(',');
-        var unused = array.pop();
-        var max = array.pop();
-        var min = array.pop();
-        alert(unused +',' + min +',' + max);
+        array.pop();
+
+        var oneFrameSize = Globals.OscillatorsNumber + 2;
+        var count = array.length / oneFrameSize;
+
+        for (var i = 0; i < count; i++) {
+            var current = array.slice(i * oneFrameSize, (i + 1) * oneFrameSize);
+            var max = current.pop();
+            var min = current.pop();
+            parser.parse(current, min, max, readyData);
+            alert(readyData);
+        }
+
+        return readyData;
     }
 };
